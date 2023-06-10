af6fddfe995c30cc0b7ef3a36b312e37f4556279
DasMukut <Mukut.Das@rishabhsoft.com>
2023-06-10 10:22:50 +0530

    Structure changes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import User from './user';
+import Home from "./components/Home"
 function App() {
   return (
     <div className="App">
         
-        <User data={{name:"MukutDas", age:22 }}/>
+       <Home />
     </div>
   );
 }
